3162647b0ce850f40057cbd51c567d92ffcd561a
Awwie <acecace@live.com>
2022-03-31 07:30:36 -0500

    commit tree 119b3403769d9025e9e94575c827f2f82900de3b

--- app.rb
+++ app.rb
@@ -1,9 +1,58 @@
 require './environment'
 
+
+require './environment'
+
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    # post '/pirates' do
+    #   params["pirate"]["ships"].each do |ship_data|
+    #     Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+    #   end
+
+    #   @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+    #   @ships = Ship.all
+
+    #   erb :'pirates/show'
+    # end
+
+    post '/pirates' do 
+
+      # @pirate = Pirate.new(params[:pirate])
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details[:name], details[:type], details[:booty])
+      end 
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+
+    end 
+
+
+    # post '/student' do
+    #   @student = Student.new(params[:student])
+    
+    #   params[:student][:courses].each do |details|
+    #     Course.new(details)
+    #   end
+    
+    #   @courses = Course.all
+    
+    #   erb :student
+    # end 
   end
 end
